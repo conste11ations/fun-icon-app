0529f02e3bac87fb2b51a76c7ee247ce1368c1a9
Rachel Kat <rachelkat@gmail.com>
2020-07-13 15:51:47 -0400

    Updates

---
+++ precache-manifest.738f8bd2aabe8527199c470fe891e4cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b747cd3a78ce68481e0f808704056034",
+    "url": "/fun-icon-app/index.html"
+  },
+  {
+    "revision": "1fefb39400ee2fb90f33",
+    "url": "/fun-icon-app/static/js/2.0b0cffa1.chunk.js"
+  },
+  {
+    "revision": "c8a4a051cf0d667b7f8d94baeb475206",
+    "url": "/fun-icon-app/static/js/2.0b0cffa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c76c4834b5788e202d7",
+    "url": "/fun-icon-app/static/js/main.4a3e8fb0.chunk.js"
+  },
+  {
+    "revision": "78b1c886a95bd1399182",
+    "url": "/fun-icon-app/static/js/runtime-main.e06fdcd5.js"
+  },
+  {
+    "revision": "c829d2db744afec1d65f5756a4572003",
+    "url": "/fun-icon-app/static/media/background-cooltone.c829d2db.png"
+  }
+]);
