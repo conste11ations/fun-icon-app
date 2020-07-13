32f4dc0cf3159f8c2650a565e9d058423f6e4e44
Rachel Kat <rachelkat@gmail.com>
2020-07-13 02:06:54 -0400

    new IconList boilerplate component and icon images

---
+++ src/components/IconList.tsx
@@ -0,0 +1,28 @@
+// prettier-ignore
+import { makeStyles } from '@material-ui/core';
+import * as React from 'react';
+
+interface Props {
+
+}
+
+function IconList(props: Props) {
+
+const {  } = props;
+const classes = useStyles();
+
+return (
+<div className={classes.root}>
+<div />
+</div>
+);
+}
+
+const useStyles = makeStyles({
+
+root: {
+
+},
+});
+
+export default IconList;
